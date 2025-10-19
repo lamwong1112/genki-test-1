fd9d61269ca160537ac0bd6cf905064184900e7b
lamwong1112 <deep1112@hotmail.com>
2025-10-20 03:43:17 +0800

    Change default theme from dark to system mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,13 +30,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="dark" suppressHydrationWarning>
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <ThemeProvider
           attribute="class"
-          defaultTheme="dark"
+          defaultTheme="system"
           enableSystem={true}
           disableTransitionOnChange
         >
